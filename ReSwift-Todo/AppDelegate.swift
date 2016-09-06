18e1afbcb080ba3883852459252b3e4dee117944
Christian Tietze <christian.tietze@gmail.com>
2016-09-06 09:50:18 +0200

    Initial Commit

---
+++ ReSwift-Todo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ReSwift-Todo
+//
+//  Created by Christian Tietze on 06/09/16.
+//  Copyright © 2016 Christian Tietze. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
